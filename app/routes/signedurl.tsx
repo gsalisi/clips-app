4a1b972b80a49246c16f4e264076b4f6c46ab951
Geoffrey Salisi <geoffreysalisi@gmail.com>
2023-04-09 15:02:20 -0700

    s3 uploads working, user id to str

--- app/routes/signedurl.tsx
+++ app/routes/signedurl.tsx
@@ -1,31 +1,33 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import AWS from "aws-sdk";
+import { requireUserId } from "~/session.server";
+// import { v4 as uuidv4 } from 'uuid';
 
 const s3 = new AWS.S3()
+const CLIPS_S3_BUCKET_NAME = "clips-store-5a1a17e"
+const CLIPS_S3_UPLOAD_PATH = "tmp"
 
 export async function loader({ request }: LoaderArgs) {
-    const randomID = Math.random() * 10000000
-    const Key = `${randomID}.jpg`
+    const userId = await requireUserId(request);
+    const url = new URL(request.url)
+    const objectName = url.searchParams.get('objectName')
+    const contentType = url.searchParams.get('contentType')
 
     // Get signed URL from S3
     const s3Params = {
-        Bucket: "iceborg-cold-store-dev",
-        Key,
+        Bucket: CLIPS_S3_BUCKET_NAME,
+        Key: `${CLIPS_S3_UPLOAD_PATH}/${userId}/${objectName}`,
         Expires: 300,
-        ContentType: 'image/jpeg',
-
-        // This ACL makes the uploaded object publicly readable. You must also uncomment
-        // the extra permission for the Lambda function in the SAM template.
-
-        // ACL: 'public-read'
+        ContentType: contentType,
+        ACL: 'private'
     }
 
     console.log('Params: ', s3Params)
     const uploadURL = await s3.getSignedUrlPromise('putObject', s3Params)
 
     return json({
-        uploadURL: uploadURL,
-        Key
+        signedUrl: uploadURL,
+        Key: s3Params.Key,
     })
 }
