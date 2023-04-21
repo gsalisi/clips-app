9ec2234af32ffdb132298b1e978f473f462444c6
Geoffrey Salisi <geoffreysalisi@gmail.com>
2023-04-21 03:14:45 -0700

    add hook for testing tracker

--- app/sqs.server.ts
+++ app/sqs.server.ts
@@ -1,10 +1,11 @@
 import AWS, { SQS } from "aws-sdk";
 import { bool } from "aws-sdk/clients/signer";
+import cuid from "cuid";
 import path from "node:path";
 
 // This is in camel-case because this is consumed in the python code
-type ClipsCorePayload = {
-  type: "crop" | "track";
+type CropperSQSPayload = {
+  type: "crop";
   bucket: string;
   input_key: string;
   output_key: string;
@@ -15,16 +16,26 @@ type ClipsCorePayload = {
   exclude_limbs: bool;
 };
 
+type TrackerSQSPayload = {
+    type: "track";
+    bucket: string;
+    input_key: string;
+    track_dest: string;
+    track_preview_dir: string;
+};
+
 const QUEUE_URL = "https://sqs.us-west-2.amazonaws.com/872511653058/cropper_queue-a871fe0.fifo"
 
+// TODO: Use explicit dedupe id for project ?
 export const sendSqsMessage = async (
-  payload: ClipsCorePayload
+  payload: CropperSQSPayload | TrackerSQSPayload
 ): Promise<SQS.SendMessageResult> => {
   for (let value of Object.values(payload)) {
     if (!value) {
       throw Error(`All parameters required. ${value} does not exist.`);
     }
   }
+
   const params = {
     MessageAttributes: {
       Action: {
@@ -33,7 +44,7 @@ export const sendSqsMessage = async (
       },
     },
     MessageBody: JSON.stringify(payload),
-    MessageDeduplicationId: payload.output_key, // Required for FIFO queues
+    MessageDeduplicationId: cuid(), // Required for FIFO queues
     MessageGroupId: "Group1", // Required for FIFO queues
     QueueUrl: QUEUE_URL,
   };
