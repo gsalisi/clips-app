7385e4243f6ecada7317fc668b71d8fec5357998
Geoffrey Salisi <geoffreysalisi@gmail.com>
2023-05-03 02:01:45 -0700

    remove processing options

---
+++ app/components/ProcessingOptions.tsx
@@ -0,0 +1,27 @@
+// import { useRevalidator } from "@remix-run/react";
+// import { useEffect, useRef, useState } from "react";
+// import type { Project, TrackHint } from "~/models/project.server";
+
+// export default function FrameAnnotation({
+//   video,
+//   addFocus,
+//   existingTrackHints,
+// }: {
+//   video: HTMLVideoElement;
+//   existingTrackHints?: TrackHint[];
+//   addFocus: (t: TrackHint) => void;
+// }) {
+//   const canvasRef = useRef<HTMLCanvasElement>(null);
+//   const overlayRef = useRef<HTMLCanvasElement>(null);
+
+//   return (
+//     <div className="w-full max-w-lg">
+//       <label className="label">
+//         <span className="label-text">
+//           {"Click on the person that you want to crop in to:"}
+//         </span>
+//       </label>
+      
+//     </div>
+//   );
+// }
