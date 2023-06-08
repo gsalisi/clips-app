686351fce212116564eeabc1eb154c9773dd0256
Geoffrey Salisi <geoffreysalisi@gmail.com>
2023-06-07 23:17:55 -0700

    fix feedback form

--- app/routes/support.tsx
+++ app/routes/support.tsx
@@ -4,12 +4,10 @@ export default function IndexPage() {
   const user = useOptionalUser();
   return (
     <iframe
+      title="Feedback Form"
       src="https://docs.google.com/forms/d/e/1FAIpQLSexXwj2X1B0vRCG810aTXGtriUvYchjyRjkzLzuhSSufTJRlg/viewform?embedded=true"
       width="100%"
       height="100%"
-      frameborder="0"
-      marginheight="0"
-      marginwidth="0"
     >
       Loading…
     </iframe>
