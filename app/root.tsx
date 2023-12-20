c86c7d8fa42494e674c2674717692b7adf90d12c
Geoffrey Salisi <geoffreysalisi@gmail.com>
2023-12-19 16:43:18 -0800

    Add Google Analytics

--- app/root.tsx
+++ app/root.tsx
@@ -31,6 +31,17 @@ export default function App() {
         <Meta />
         <Links />
       </head>
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-5KMQGRZLT6"></script>
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-5KMQGRZLT6');
+        `,
+      }}/>
       <body className="absolute top-0 bottom-0 w-full">
         <Outlet />
         <ScrollRestoration getKey={(loc) => loc.pathname} />
